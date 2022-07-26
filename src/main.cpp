30c6c2555ce6a69f7cbce0734be06be701b24b11
wojtek <wojtek.zajaczkowski@gmail.com>
2022-07-26 09:39:01 +0200

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,11 @@
 #include <BLEServer.h>
 #include <BLEUtils.h>
 #include <BLE2902.h>
-#include "sys/time.h"
-
-#include "sensor.h"
 #include <EEPROM.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include "sensor.h"
+#include "driver/adc.h"
 
 template <class T> int EEPROM_writeAnything(int ee, const T& value){
   ee+=0;
@@ -72,14 +72,124 @@ void init_flash(){
 }
 
 void init_Sensor(){
-  SensorInfo.ALARM        = false;
-  SensorInfo.Resistance   = 0;
-  SensorInfo.Temperature  = 0;
-  SensorInfo.BatteryLevel = 0;
-
+  SensorInfo.ALARM         = false;
+  SensorInfo.Resistance    = 0;
+  SensorInfo.ResistanceMAX = 0;
+  SensorInfo.Temperature   = 0;
+  SensorInfo.BatteryLevel  = 0;
 }
+
+
+void PrintSensor(){
+  Serial.printf(":: Resistance: %d \t %d \n", SensorInfo.Resistance, SensorInfo.ResistanceMAX);
+}
+
+
+//Setup callbacks onConnect and onDisconnect
+class MyServerCallbacks: public BLEServerCallbacks {
+  void onConnect(BLEServer* pServer) {
+    deviceConnected = true;
+  };
+  void onDisconnect(BLEServer* pServer) {
+    deviceConnected = false;
+    pServer->getAdvertising()->start();
+  }
+};
+class Sesnor_Status_Callbacks: public BLECharacteristicCallbacks {
+
+    void onWrite(BLECharacteristic *pCharacteristic)
+    {
+      uint32_t* received_data = pCharacteristic->getData();
+      Serial.println("Sesnor_Status_Callbacks >> onWrite ");
+      Serial.print("Sesnor_Status_Callbacks >>");
+      Serial.println(*received_data);
+      pCharacteristic.
+    }
+
+
+    
+
+
+};
+
+void init_BLE(){
+   // Create the BLE Device 
+  BLEDevice::init("MyESP32");
+
+  // Create the BLE Server
+  BLEServer  *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(BLE_UUID_SENSOR_SERVICE);
+
+
+  // Create BLE Characteristics and Create a BLE Descriptor
+  // STATUS
+  pService->addCharacteristic(&Characteristic_Sesnor_Status);
+  Descripto_Sesnor_Status.setValue("Sesnor Status");
+  Characteristic_Sesnor_Status.addDescriptor(&Descripto_Sesnor_Status);
+  Characteristic_Sesnor_Status.setCallbacks(new Sesnor_Status_Callbacks());
+  // Characteristic_Sesnor_Status.setValue("666");
+  // // Value
+  // pService->addCharacteristic(&Characteristic_Sesnor_Value);
+  // Descripto_Sesnor_Value.setValue("Sesnor Value");
+  // Characteristic_Sesnor_Value.addDescriptor(&Descripto_Sesnor_Value);
+
+  // // Value SET
+  // pService->addCharacteristic(&Characteristic_Sesnor_Value_Set);
+  // Descripto_Sesnor_Value_Set.setValue("Sesnor Value Set");
+  // Characteristic_Sesnor_Value_Set.addDescriptor(&Descripto_Sesnor_Value_Set);
+
+  // // ALARM
+  // pService->addCharacteristic(&Characteristic_Sesnor_Alarm);
+  // Descripto_Sesnor_Alarm.setValue("Sesnor Alarm");
+  // Characteristic_Sesnor_Alarm.addDescriptor(&Descripto_Sesnor_Alarm);
+
+  // // Value
+  // pService->addCharacteristic(&Characteristic_Sesnor_Alarm_Reset);
+  // Descripto_Sesnor_Alarm_Reset.setValue("Sesnor Alarm Reset");
+  // Characteristic_Sesnor_Alarm_Reset.addDescriptor(&Descripto_Sesnor_Alarm_Reset);
+
+  // // Update Period
+  // pService->addCharacteristic(&Characteristic_Sesnor_Update_Period);
+  // Descripto_Sesnor_Update_Period.setValue("Sesnor Update Period");
+  // Characteristic_Sesnor_Update_Period.addDescriptor(&Descripto_Sesnor_Update_Period);
+
+  // // Reset
+  // pService->addCharacteristic(&Characteristic_Sesnor_Reset);
+  // Descripto_Sesnor_Reset.setValue("Sesnor Alarm Reset");
+  // Characteristic_Sesnor_Reset.addDescriptor(&Descripto_Sesnor_Reset);
+
+  // Temperature
+  // pService->addCharacteristic(&Characteristic_Sesnor_Temperature);
+  // Descripto_Sesnor_Temperature.setValue("Sesnor Temperature");
+  // Characteristic_Sesnor_Temperature.addDescriptor(&Descripto_Sesnor_Temperature);
+
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  // BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  // pAdvertising->addServiceUUID(BLE_UUID_SENSOR_SERVICE);
+  // pServer->getAdvertising()->start();
+
+  pAdvertising = pServer->getAdvertising();
+  advert.setName("ESP32-new");
+  pAdvertising->setAdvertisementData(advert);
+  pAdvertising->start();
+}
+
+
+
+
+
+
 void setup() {
   Serial.begin(9600);
+  init_pin();
+  init_BLE();
   Serial.println(":: >>> Boot <<<");
   Serial.printf(":: ESP.getChipCores:     %d\n", ESP.getChipCores());
   Serial.printf(":: ESP.getChipModel:     %d\n", ESP.getChipModel());
@@ -88,15 +198,27 @@ void setup() {
   Serial.printf(":: ESP.getEfuseMac:      %d\n", ESP.getEfuseMac());
   Serial.printf(":: ESP.getFlashChipMode: %d\n", ESP.getFlashChipMode());
   Serial.printf(":: ESP.getFlashChipSize: %d\n", ESP.getFlashChipSize());
+
+uint32_t Freq = 0;
+  // setCpuFrequencyMhz(10);
+  Freq = getCpuFrequencyMhz();
+  Serial.print("CPU Freq = ");
+  Serial.print(Freq);
+  Serial.println(" MHz");
+  Freq = getXtalFrequencyMhz();
+  Serial.print("XTAL Freq = ");
+  Serial.print(Freq);
+  Serial.println(" MHz");
+  Freq = getApbFrequency();
+  Serial.print("APB Freq = ");
+  Serial.print(Freq);
+  Serial.println(" Hz");
+  adc_power_off();
 }
 
-int64_t time_get_time() {
-	struct timeval tv;
-	gettimeofday(&tv, NULL);
-	return (tv.tv_sec * 1000LL + (tv.tv_usec / 1000LL));
-}
+
 void loop() {
-
+  // PrintSensor();
   if (Sensor_Resistance_Value_expired){
     Sensor_ResistanceUpdate();
     Sensor_Resistance_Value_expired = false;
@@ -141,6 +263,8 @@ void loop() {
 
   }
 
-
-  delay(10);
+// esp_sleep_enable_timer_wakeup(5*1000000 );
+// delay(100);
+// esp_light_sleep_start();
+  delay(1000);
 }
--- src/sensor.h
+++ src/sensor.h
@@ -6,17 +6,17 @@ RTC_DATA_ATTR static time_t last;           // remember last boot in RTC Memory
 volatile uint16_t Vin = 3000; // mV  Main voltage supply on voltage divider; 
 volatile uint16_t Sensor_Resistance_CheckPeriod   = 1;   // evry 1 second
 volatile uint16_t Sensor_Resistance_LastCheck     = 0;    
-volatile bool     Sensor_Resistance_Value_expired = true;
+volatile bool     Sensor_Resistance_Value_expired = false;
 
 volatile uint16_t Sensor_Battery_CheckPeriod   = 1;      // every 30 sec. 
 volatile uint16_t Sensor_Battery_LastCheck     = 0;
-volatile bool     Sensor_Battery_Value_expired = true;
+volatile bool     Sensor_Battery_Value_expired = false;
  
 volatile uint16_t Sensor_Temperature_CheckPeriod   = 1;      // every 30 sec. 
 volatile uint16_t Sensor_Temperature_LastCheck     = 0;
-volatile bool     Sensor_Temperature_Value_expired = true;
+volatile bool     Sensor_Temperature_Value_expired = false;
 
-volatile bool     Sensor_UpdateAdvertisingData = true;
+volatile bool     Sensor_UpdateAdvertisingData = false;
 bool ALARM_MODE = false;
 
 
@@ -40,11 +40,50 @@ volatile uint32_t lastAlarm = 0;
 #define BLE_UUID_SENSOR_RESET         "db68db01-e0dd-411d-859d-32a2ef68811g"
 #define BLE_UUID_SENSOR_TEMPERATURE   "0x2A1E"
 
+BLEAdvertisementData advert;
+BLEAdvertising *pAdvertising;
+
+// Create BLE Descriptor
+// STATUS
+BLECharacteristic Characteristic_Sesnor_Status(BLE_UUID_SENSOR_STATUS, BLECharacteristic::PROPERTY_NOTIFY|BLECharacteristic::PROPERTY_WRITE);
+BLEDescriptor     Descripto_Sesnor_Status(BLEUUID((uint16_t)0x2902));
+// Value
+BLECharacteristic Characteristic_Sesnor_Value(BLE_UUID_SENSOR_VALUE, BLECharacteristic::PROPERTY_NOTIFY);
+BLEDescriptor     Descripto_Sesnor_Value(BLEUUID((uint16_t)0x2902));
+// Value_set
+BLECharacteristic Characteristic_Sesnor_Value_Set(BLE_UUID_SENSOR_VALUE_SET, BLECharacteristic::PROPERTY_NOTIFY);
+BLEDescriptor     Descripto_Sesnor_Value_Set(BLEUUID((uint16_t)0x2902));
+// ALARM
+BLECharacteristic Characteristic_Sesnor_Alarm(BLE_UUID_SENSOR_ALARM, BLECharacteristic::PROPERTY_NOTIFY);
+BLEDescriptor     Descripto_Sesnor_Alarm(BLEUUID((uint16_t)0x2902));
+// ALARM
+BLECharacteristic Characteristic_Sesnor_Alarm_Reset(BLE_UUID_SENSOR_ALARM_RESET, BLECharacteristic::PROPERTY_NOTIFY);
+BLEDescriptor     Descripto_Sesnor_Alarm_Reset(BLEUUID((uint16_t)0x2902));
+// UPDATE_PERIOD
+BLECharacteristic Characteristic_Sesnor_Update_Period(BLE_UUID_SENSOR_UPDATE_PERIOD, BLECharacteristic::PROPERTY_NOTIFY);
+BLEDescriptor     Descripto_Sesnor_Update_Period(BLEUUID((uint16_t)0x2902));
+// RESET
+BLECharacteristic Characteristic_Sesnor_Reset(BLE_UUID_SENSOR_RESET, BLECharacteristic::PROPERTY_NOTIFY);
+BLEDescriptor     Descripto_Sesnor_Reset(BLEUUID((uint16_t)0x2902));
+// TEMPERATURE
+BLECharacteristic Characteristic_Sesnor_Temperature(BLE_UUID_SENSOR_TEMPERATURE, BLECharacteristic::PROPERTY_NOTIFY);
+BLEDescriptor     Descripto_Sesnor_Temperature(BLEUUID((uint16_t)0x2902));
+
+bool deviceConnected = false;
+
+
+
+
+
+
+
+
 //define EEPROM size
 #define EEPROMSize  512
 
 struct Info{
     volatile uint32_t Resistance;
+    volatile uint32_t ResistanceMAX;
     volatile float_t Temperature;
     volatile uint32_t BatteryLevel;
     volatile uint32_t UpTime;
